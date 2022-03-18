5f0eb0379d0eb07577a7af26d5450aec04834e03
luca.lorenzetti <luca.lorenzetti0@gmail.com>
2022-03-18 22:00:24 +0100

    change yay with paru

--- install.sh
+++ install.sh
@@ -29,33 +29,32 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 ### Check if user is root
 
-#if [ "$EUID" -ne 0 ]
-#  then echo "Please run as root"
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
   
-#  exit
-#fi
+  exit
+fi
 
 CURRENTUSER=$LOGNAME
-
-su 
+ 
 ### Update system
 echo "Update the system"
 
 pacman -Syy -y --noconfirm && pacman -Syu --noconfirm
 
-#### Install yay
-echo "Installation yay"
+#### Install paru
+echo "Installation paru"
 
-# 1) install git and base-devel for yay
+# 1) install git and base-devel for paru
 	echo "install git and  base-devel"
 	pacman -S --needed git base-devel --noconfirm
 
-# 2) Clone repo and create folder yay
-	git clone https://aur.archlinux.org/yay.git /home/$CURRENTUSER/
+# 2) Clone repo and create folder paru
+	git clone https://aur.archlinux.org/paru.git /home/$CURRENTUSER/
 	
-	# enter into yay folder and make
-	cd yay
-	sudo -u nobody makepkg -si
+	# enter into paru folder and make
+	cd paru
+	makepkg -si
 	
 ####  Install packages from packages.list (PACMAN)
 
