a2e4d7b3d44e8a8158f0ff6ed17b1ac4a7550bcb
luca lorenzetti <luca.lorenzetti0@gmail.com>
2022-04-23 22:16:09 +0200

    first test

--- arch_install.sh
+++ arch_install.sh
@@ -5,3 +5,45 @@ echo "////////////////////////////////////////////
 //////////////////////////////////////////////////"
 
 pacman -S parted --noconfirm
+
+
+# delete partition
+for i in 10
+do
+	parted -s /dev/sda rm $i
+done
+
+
+#Create partition
+
+parted -s /dev/sda mkpart primary ext4 0% 100%
+
+
+mkfs.ext4 /dev/sda1
+
+pacman -Syy
+
+mount /dev/sda1 /mnt
+
+pacstrap /mnt base linux linux-firmware sudo nano
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+
+echo 'it_IT.UTF-8 UTF-8' >> /etc/locale.gen
+
+locale-gen
+
+echo LANG=it_IT.UTF-8 > /etc/locale.conf
+export LANG=it_IT.UTF-8
+
+echo arch-test > /etc/hostname
+
+echo "root:root" | chpasswd -e
+
+pacman -S grub efibootmgr
+
+
